8ddae3ad3eb9f8641709ee385f72cbb284a361b6
VeldiRahul <142107586+VeldiRahul@users.noreply.github.com>
2025-04-12 17:18:37 +0530

    Add files via upload

---
+++ Mux_2x1/mux2x1.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Korrapolu Eswar Adithya
+// 
+// Create Date: 24.11.2024 00:41:40
+// Design Name: 
+// Module Name: mux2x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2x1(output y, input [1:0]x, input s, input en);
+
+    //Logic
+   assign y = en ? (s ? x[1] : x[0]) : 1'bx;
+    
+endmodule
