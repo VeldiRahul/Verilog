e46b64810bb9e471f199aaee3412bc888094f3b8
VeldiRahul <142107586+VeldiRahul@users.noreply.github.com>
2025-04-15 21:25:53 +0530

    Add files via upload

---
+++ FUll_Adder/Full_Adder using Half_Adder/Full_adder (2).v
@@ -0,0 +1,12 @@
+`timescale 1ns/1ps
+module full_adder(input a,b,c,output sum,carry);
+  
+  wire w1, w2, w3;
+
+  half_adder HA1(.a(a), .b(b), .sum(w1), .carry(w2));
+  half_adder HA2(.a(w1), .b(c), .sum(sum), .carry(w3));
+
+  or or1(carry, w2, w3);
+
+endmodule
+
